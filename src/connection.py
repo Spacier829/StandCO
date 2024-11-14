fd991998152ba7ae856f1b397d6bf73e23dd11fb
Spacier <mrsimpleeegor@gmail.com>
2024-11-14 22:50:47 +0700

    feat(main): added connection to sensors with ip

--- src/connection.py
+++ src/connection.py
@@ -1,2 +1,18 @@
-import pymodbus
+import json
+from pymodbus.client import ModbusTcpClient
 
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+clients = []
+
+def connect_to_sensors():
+    for sensor in config['sensors']:
+        client = ModbusTcpClient(host=sensor['ip'], port=sensor['port'])
+        if client.connect():
+            print(f"Connected to {sensor['ip']}:{sensor['port']}")
+            clients.append(client)
+        else:
+            print(f"Failed to connect to {sensor['ip']}:{sensor['port']}")
+
+connect_to_sensors()

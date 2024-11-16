818e91e74de80529b9eea8e853317df384cb8123
Spacier <mrsimpleeegor@gmail.com>
2024-11-16 17:38:54 +0700

    feat(main): moved connection to class, add simulators ip for debug

--- src/connection.py
+++ src/connection.py
@@ -1,18 +1,17 @@
 import json
 from pymodbus.client import ModbusTcpClient
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
 
-clients = []
+class Connection:
+    clients = []
 
-def connect_to_sensors():
-    for sensor in config['sensors']:
-        client = ModbusTcpClient(host=sensor['ip'], port=sensor['port'])
-        if client.connect():
-            print(f"Connected to {sensor['ip']}:{sensor['port']}")
-            clients.append(client)
-        else:
-            print(f"Failed to connect to {sensor['ip']}:{sensor['port']}")
-
-connect_to_sensors()
+    def connect_to_sensors(self):
+        with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+        for sensor in config['sensors']:
+            client = ModbusTcpClient(host=sensor['ip'], port=sensor['port'])
+            if client.connect():
+                print(f"Connected to {sensor['ip']}:{sensor['port']}")
+                self.clients.append(client)
+            else:
+                print(f"Failed to connect to {sensor['ip']}:{sensor['port']}")
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from connection import Connection
+
+if __name__ == '__main__':
+    connection = Connection()
+    connection.connect_to_sensors()
